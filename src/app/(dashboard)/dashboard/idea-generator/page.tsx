ef98dd06315fb310f00c6ced5b1726fd6fcd9447
wesselpawel <wesselpawel@wp.pl>
2024-11-03 19:16:25 +0100

    Checkpoint api try

--- src/app/(dashboard)/dashboard/idea-generator/page.tsx
+++
@@ -1,8 +0,0 @@
-import ImageGenerator from "@/components/ImageGenerator";
-
-export default function Page() {
-  return <ImageGenerator />;
-}
-export const metadata = {
-  title: "Generator Obrazów - Panel użytkownika Quixy",
-};
