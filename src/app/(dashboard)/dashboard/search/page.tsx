9c47aa14fd0c2d005f6123556eff0522dec7e31c
wesselpawel <wesselpawel@wp.pl>
2024-11-14 14:14:46 +0100

    add service

--- src/app/(dashboard)/dashboard/search/page.tsx
+++ src/app/(dashboard)/dashboard/search/page.tsx
@@ -1,38 +1,7 @@
-"use client";
-import { useState } from "react";
-import CategorySelector from "@/components/AddJobOffer/CategorySelector";
-import Postings from "@/components/Postings/Postings";
+import Search from "@/components/Search";
 import { Metadata } from "next";
-export default function Page() {
-  const [tagsOpenLevel, setTagsOpenLevel] = useState(false);
-  const [tagDeletion, setTagDeletion] = useState(false);
-  const [selectedTag, setSelectedTag] = useState("");
-  const [configurationOpen, setConfigurationOpen] = useState(false);
-  const [slug, setSlug] = useState("");
-  const [category, setCategory] = useState("");
-
-  const [formData, setFormData] = useState({});
-
-  return (
-    <div>
-      <CategorySelector
-        setTagsOpenLevel={setTagsOpenLevel}
-        tagsOpenLevel={tagsOpenLevel}
-        setTagDeletion={setTagDeletion}
-        selectedTag={selectedTag}
-        setSelectedTag={setSelectedTag}
-        tagDeletion={tagDeletion}
-        configurationOpen={configurationOpen}
-        setConfigurationOpen={setConfigurationOpen}
-        slug={slug}
-        setSlug={setSlug}
-        category={category}
-        setCategory={setCategory}
-        formData={formData}
-        setFormData={setFormData}
-      />
-    </div>
-  );
+export default async function Page() {
+  return <Search />;
 }
 export const metadata: Metadata = {
   publisher: "quixy.pl",
@@ -46,7 +15,7 @@ export const metadata: Metadata = {
   verification: {
     google: "google85185d3abec28326.html",
   },
-  title: `Twoje Oferty Pracy - Panel Użytkownika`,
+  title: `Wyszukiwarka - Panel Użytkownika`,
   description:
     "Zatrudnij ekspertów z branży IT, marketingu, designu i innych dziedzin. Znajdź specjalistów w Quixy Talent™ i rozwijaj swój biznes już dziś!",
 };
---
+++ src/components/Search.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import CategorySelector from "@/components/AddJobOffer/CategorySelector";
+import { Metadata } from "next";
+export default function Search() {
+  const [tagsOpenLevel, setTagsOpenLevel] = useState(false);
+  const [tagDeletion, setTagDeletion] = useState(false);
+  const [selectedTag, setSelectedTag] = useState("");
+  const [configurationOpen, setConfigurationOpen] = useState(false);
+  const [slug, setSlug] = useState("");
+  const [category, setCategory] = useState("");
+  const [formData, setFormData] = useState({});
+  return (
+    <div>
+      <CategorySelector
+        setTagsOpenLevel={setTagsOpenLevel}
+        tagsOpenLevel={tagsOpenLevel}
+        setTagDeletion={setTagDeletion}
+        selectedTag={selectedTag}
+        setSelectedTag={setSelectedTag}
+        tagDeletion={tagDeletion}
+        configurationOpen={configurationOpen}
+        setConfigurationOpen={setConfigurationOpen}
+        slug={slug}
+        setSlug={setSlug}
+        category={category}
+        setCategory={setCategory}
+        formData={formData}
+        setFormData={setFormData}
+      />
+    </div>
+  );
+}
