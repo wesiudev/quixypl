45465c92000635dfbce8cd255e4c2868e5fac5e3
wesselpawel <wesselpawel@wp.pl>
2024-11-26 23:38:59 +0100

    once a week

--- src/components/AboutQuixyTalent.tsx
+++ src/components/AboutQuixyTalent.tsx
@@ -2,14 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import {
-  FaUserTie,
-  FaBriefcase,
-  FaCheckCircle,
-  FaHandsHelping,
-  FaRocket,
-  FaCogs,
-} from "react-icons/fa";
+import { FaCheckCircle, FaCogs } from "react-icons/fa";
 
 export default function AboutQuixyTalent() {
   const [content, setContent] = useState("talent");
