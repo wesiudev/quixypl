5a4a2948d05fa1ef4e3792bd16eadd82ecd4adc8
wesselpawel <wesselpawel@wp.pl>
2024-11-26 23:35:04 +0100

    once a week

--- src/components/ImageGenerator.tsx
+++ src/components/ImageGenerator.tsx
@@ -1,14 +1,8 @@
 "use client";
 import { useSelector } from "react-redux";
-import Backpack from "./user/ImageGenerator/backpack/Backpack";
 import Loading from "@/app/loading";
 
 export default function ImageGenerator() {
   const { user } = useSelector((state: any) => state.user);
-  return (
-    <div>
-      {user && <Backpack user={user} />}
-      {!user && <Loading />}
-    </div>
-  );
+  return <div>{!user && <Loading />}</div>;
 }
