8e20c9cce27ea4f956f3e6ad6e8f3545c62b2780
wesselpawel <wesselpawel@wp.pl>
2024-11-26 23:17:27 +0100

    once a week

--- src/app/company/layout.tsx
+++ src/app/company/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import jobs from "../../../public/14.09.2024.json";
 import MainFooter from "@/components/MainFooter";
-import Chat from "@/components/user/Chat/Chat";
+import Chat from "@/components/Dashboard/Chat/Chat";
 export default async function Layout({
   children,
 }: {
