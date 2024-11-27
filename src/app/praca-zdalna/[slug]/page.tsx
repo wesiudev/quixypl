67f8fb6d2b7a45cfe4a6c1af12f4845594f931d2
wesselpawel <wesselpawel@wp.pl>
2024-11-27 10:44:56 +0100

    test

--- src/app/praca-zdalna/[slug]/page.tsx
+++ src/app/praca-zdalna/[slug]/page.tsx
@@ -14,11 +14,7 @@ export async function generateStaticParams() {
     slug: polishToEnglish(service.title),
   }));
 }
-export default async function Page(props: {
-  params: Promise<any>;
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const searchParams = await props?.searchParams;
+export default async function Page(props: { params: Promise<any> }) {
   const params = await props?.params;
   const slug: any = jobs?.find(
     (page: any) => polishToEnglish(page?.title) === params?.slug
@@ -26,18 +22,27 @@ export default async function Page(props: {
   const contentReq = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/content?tubylytylkofigi=${process.env.API_SECRET_KEY}&job=${params.slug}`,
     {
-      next: { revalidate: 60 },
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "GET",
     }
   );
   const content = await contentReq.json();
   // const products: any = await getProducts();
-  const isTalent = searchParams?.talent === "" ? true : false;
   const talentsReq = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/talents/slug?tubylytylkofigi=${
       process.env.API_SECRET_KEY
     }&slug=${polishToEnglish(params.slug)}`,
     {
-      next: { revalidate: 60 },
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "GET",
     }
   );
   const talents = await talentsReq.json();
@@ -46,7 +51,12 @@ export default async function Page(props: {
       process.env.API_SECRET_KEY
     }&slug=${polishToEnglish(params.slug)}`,
     {
-      next: { revalidate: 60 },
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "GET",
     }
   );
   const companies = await companiesReq.json();
@@ -65,22 +75,15 @@ export default async function Page(props: {
           <div className="w-full py-6 lg:py-12 text-center overflow-hidden relative mt-6 lg:mt-12 bg-white">
             <div className="container mx-auto">
               <p className="pb-4 lg:pb-8 px-6 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold mb-6 leading-snug w-full text-center mx-auto bg-gradient-to-r from-primary to-cta text-transparent bg-clip-text">
-                Freelancer Job Boards – {slug.title}
+                Freelancer Job Boards – {slug?.title}
               </p>
             </div>
-            {/* <h2 className="text-2xl font-semibold mb-6">{slug.h2}</h2> */}
-            {isTalent && (
-              <p className="-mt-6 font-coco text-black w-full sm:text-lg lg:max-w-xl mx-auto">
-                Zaprezentuj swoje usługi i znajdź pracę by rozwinąc swoje
-                portfolio.
-              </p>
-            )}
-            {!isTalent && (
-              <p className="-mt-6 font-coco text-black w-full sm:text-lg lg:max-w-xl mx-auto">
-                Stwórz najskuteczniejsze oferty pracy w {content?.genitive}!
-                Zajmij się swoim biznesem, a my znajdziemy idealnych ekspertów.
-              </p>
-            )}
+
+            <p className="-mt-6 font-coco text-black w-full sm:text-lg lg:max-w-xl mx-auto">
+              Stwórz najskuteczniejsze oferty pracy w {content?.genitive} lub
+              zaprezentuj swoje usługi i zdobądź klientów.
+            </p>
+
             <div className="mt-2"></div>
             <Link href="/register" className="relative z-50">
               <button className="bg-gradient-to-r from-primary to-cta text-white px-2 py-1.5">
@@ -92,7 +95,7 @@ export default async function Page(props: {
         <div className="bg-gradient-to-r from-primary to-cta w-full h-full mx-auto pt-6 lg:pt-12">
           <div className="px-6 lg:px-12 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-12">
             <Image
-              src={`/slug/${polishToEnglish(slug.title)}.webp`}
+              src={`/slug/${polishToEnglish(slug?.title)}.webp`}
               width={1024}
               height={1024}
               alt={`Prace Zdalne w ${content?.genitive}`}
@@ -102,21 +105,16 @@ export default async function Page(props: {
             />
             <div className="flex flex-col w-full">
               <h1 className="font-extrabold text-2xl lg:text-4xl text-white drop-shadow-md shadow-black mb-6">
-                {isTalent && "Szukaj pracy zdalnej"}{" "}
-                {!isTalent && "Dodaj ofertę pracy"} w {content?.genitive}.{" "}
+                Praca, zlecenia, usługi i oferty w {content?.genitive}.{" "}
               </h1>
-              <div
-                className={`${
-                  isTalent ? "flex-col-reverse" : "flex-col"
-                } flex mt-3`}
-              >
+              <div className={`flex flex-col mt-3`}>
                 <div className="p-6 text-black bg-white">
                   <h2 className="text-black w-max font-extrabold">
                     Dla klientów
                   </h2>
                   <p className="sm:text-lg 2xl:text-xl pb-3 text-black">
                     Zatrudnij najlepszych specjalistów — opublikuj ofertę pracy
-                    w kategorii <b className="font-bold">{slug.title}</b> i
+                    w kategorii <b className="font-bold">{slug?.title}</b> i
                     znajdź ekspertów w tej dziedzinie. Promuj swoje usługi, by
                     dotrzeć do odpowiednich odbiorców.
                   </p>
@@ -172,12 +170,12 @@ export default async function Page(props: {
               >
                 Zlecenia i oferty pracy zdalnej dla freelancerów oraz firm
               </h2>
-              {jobs.map((job: any, i: any) => (
+              {jobs?.map((job: any, i: any) => (
                 <div key={i}>
                   {polishToEnglish(job.title) === params.slug && (
                     <div className="flex flex-col" key={i}>
                       <div className="flex flex-col bg-white rounded-xl gap-3">
-                        {job.data.map((item: any, j: any) => (
+                        {job?.data?.map((item: any, j: any) => (
                           <div
                             key={j}
                             className="relative bg-gradient-to-r from-transparent to-primary/10 p-3 rounded-xl"
@@ -191,7 +189,7 @@ export default async function Page(props: {
 
                             {/* Hover dropdown */}
                             <div className="flex w-[90%] flex-wrap">
-                              {item.data.map((subcategory: any, k: any) => (
+                              {item?.data?.map((subcategory: any, k: any) => (
                                 <Link
                                   title={`Pracuj zdalnie w ${subcategory.title}`}
                                   key={k}
@@ -217,7 +215,8 @@ export default async function Page(props: {
             </div>
             <section className="w-full lg:w-[50%]">
               <h2 className="pb-3 text-black text-2xl font-extrabold">
-                Czym zajmują się {content?.informal_title_plural.toLowerCase()}?
+                Czym zajmują się {content?.informal_title_plural?.toLowerCase()}
+                ?
               </h2>
 
               <div
@@ -231,21 +230,21 @@ export default async function Page(props: {
             </section>
           </div>
           {/* <h2 className="text-xl font-semibold text-primary mb-4">
-          Najlepsi specjaliści {slug.title}
+          Najlepsi specjaliści {slug?.title}
         </h2> */}
-          {/* display users with seek:true and user?.categories includes slug.title, else display "no users, want to be first? man with black glasses italic" */}
+          {/* display users with seek:true and user?.categories includes slug?.title, else display "no users, want to be first? man with black glasses italic" */}
         </div>
         <div className="bg-white px-6 lg:px-12 py-12 flex flex-col w-full text-black">
           <h4 className="text-lg w-max font-extrabold">Tagi</h4>
           <ul className="font-coco flex items-center flex-wrap">
             {content?.synonyms?.map((item: any, i: any) => (
               <li key={i} className={`ml-2 mt-2`}>
-                #{removePolishSignsAndSpaces(item.toLowerCase())}
+                #{removePolishSignsAndSpaces(item?.toLowerCase())}
               </li>
             ))}
 
-            {isTalent && <li className="mt-2 ml-2">#znajdzprace</li>}
-            {!isTalent && <li className="mt-2 ml-2">#rekrutacja</li>}
+            <li className="mt-2 ml-2">#znajdzprace</li>
+            <li className="mt-2 ml-2">#rekrutacja</li>
             <li className="mt-2 ml-2">#pracazdalna</li>
             <li className="mt-2 ml-2">#firmy{content?.genitive}</li>
             <li className="mt-2 ml-2">#freelancer</li>
@@ -255,16 +254,16 @@ export default async function Page(props: {
             <li className="mt-2 ml-2">#ogloszeniaoprace</li>
             <li className="mt-2 ml-2">#ogloszeniapracy</li>
             <li className="mt-2 ml-2">
-              #{removePolishSignsAndSpaces(slug.title.toLowerCase())}
+              #{removePolishSignsAndSpaces(slug?.title?.toLowerCase())}
             </li>
           </ul>
         </div>
       </div>
 
       <SlugFooter
-        jobsList={slug.data}
-        title={slug.title}
-        footerTitle={slug.title}
+        jobsList={slug?.data}
+        title={slug?.title}
+        footerTitle={slug?.title}
       />
     </>
   );
@@ -275,7 +274,7 @@ export async function generateMetadata(props: { params: Promise<any> }) {
   const slug: any = jobs.find(
     (page: any) => polishToEnglish(page.title) === params.slug
   );
-  const content = await getPageContent(polishToEnglish(slug.title));
+  const content = await getPageContent(polishToEnglish(slug?.title));
   const title = `Oferty pracy w ${content?.genitive} - Job Boards`;
   const description = `Prowadzisz rekrutację lub szukasz pracy w ${content?.genitive}? Chcesz zająć się ${content?.instrumental}? Mamy dla Ciebie zlecenia.`;
 
@@ -284,7 +283,7 @@ export async function generateMetadata(props: { params: Promise<any> }) {
     description,
     openGraph: {
       type: "website",
-      url: `https://quixy.pl/praca-zdalna/${polishToEnglish(slug.title)}`,
+      url: `https://quixy.pl/praca-zdalna/${polishToEnglish(slug?.title)}`,
       title,
       description,
       siteName: "Quixy",
