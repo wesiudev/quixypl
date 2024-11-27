b23772322b8d44fb92c192fe8d677834399d2134
wesselpawel <wesselpawel@wp.pl>
2024-11-27 12:09:25 +0100

    src

--- src/app/api/talents/slug/route.ts
+++ src/app/api/talents/slug/route.ts
@@ -18,7 +18,9 @@ export async function GET(req: NextRequest) {
         user?.configured &&
         user?.name &&
         user?.access &&
-        user?.tags?.filter((tag: any) => tag?.slugUrl === slug).length > 0
+        user?.tags?.filter(
+          (tag: any) => (tag?.slugUrl || tag?.url || tag?.categoryUrl) === slug
+        ).length > 0
     )
     .map((user: any) => ({
       seek: user?.seek,
