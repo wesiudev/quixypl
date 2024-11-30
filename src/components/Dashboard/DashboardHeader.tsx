33573e3db157ec8b43b585cd48c1ded4e626906d
wesselpawel <wesselpawel@wp.pl>
2024-11-30 16:34:35 +0100

    test

--- src/components/Dashboard/DashboardHeader.tsx
+++ src/components/Dashboard/DashboardHeader.tsx
@@ -41,7 +41,7 @@ export default function DashboardHeader({ users }: { users: any }) {
   }, []);
   return (
     <div>
-      <Settings source={user} setSource={setUserData} data={userData} />
+      <Settings source={user} setSource={setUserData} />
       <QuixiesModule />
       <div className="">
         <div
