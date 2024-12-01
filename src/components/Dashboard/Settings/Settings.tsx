60469f61cd84df16e4bad3bf2254934429669f6c
wesselpawel <wesselpawel@wp.pl>
2024-12-01 18:54:15 +0100

    added components

--- src/components/Dashboard/Settings/Settings.tsx
+++ src/components/Dashboard/Settings/Settings.tsx
@@ -1,12 +1,11 @@
 "use client";
 import UserEditDashboard from "./SettingsInputs/SettingsInputs";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Unsaved from "./SettingsInputs/Unsaved";
 import { updateUser } from "@/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { set_modals } from "@/redux/slices/modalsopen";
 export default function Settings() {
-  const wrapperRef = useRef<any>(null);
   const [error, setError] = useState(false);
   const [changesWereMade, setChangesWereMade] = useState(false);
   const { modals } = useSelector((state: any) => state.modals);
@@ -18,10 +17,12 @@ export default function Settings() {
       <button
         disabled={error}
         onClick={() => {
-          if (changesWereMade) {
-          } else {
-            dispatch(set_modals({ ...modals, config: !modals.config }));
-          }
+          changesWereMade
+            ? setError(true)
+            : dispatch(set_modals({ ...modals, config: !modals.config }));
+          setTimeout(() => {
+            setError(false);
+          }, 1200);
         }}
         className={`disabled:cursor-not-allowed fixed z-[999999999999999999] left-0 top-0 w-screen h-screen ${
           modals.config
@@ -30,7 +31,6 @@ export default function Settings() {
         }  bg-black hover:bg-opacity-60`}
       />
       <div
-        ref={wrapperRef}
         className={`h-[80vh] w-[95vw] lg:w-[80vw] overflow-y-scroll scrollbarChat ${
           modals.config
             ? "z-[99999999999999999] opacity-100 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
