60e1f5d55594b665e5ba71bbacca689637e5d1f4
wesselpawel <wesselpawel@wp.pl>
2024-12-01 17:07:58 +0100

    added components

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -2,15 +2,18 @@
 import { auth } from "@/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import LoginPage from "./LoginPage";
-import Nav from "./Nav";
+
 import Loading from "./loading";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import InitUser from "./InitUser";
+import Nav from "./Nav";
+import { useState } from "react";
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isNavOpen, setNavOpen] = useState(true);
   const [user, loading] = useAuthState(auth);
   if (loading) {
     return <Loading />;
@@ -20,7 +23,7 @@ export default function AdminLayout({
         {user ? (
           <>
             <InitUser user={user} />
-            <Nav />
+            <Nav setNavOpen={setNavOpen} isNavOpen={isNavOpen} />
             <div className="pl-[300px] min-w-full min-h-screen bg-[#222430]">
               {children}
             </div>
