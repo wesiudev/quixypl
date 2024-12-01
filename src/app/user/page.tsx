a9ef200de12ed6fb87baca4f34f2d3ea647af2f9
wesselpawel <wesselpawel@wp.pl>
2024-12-01 17:21:05 +0100

    added components

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div>
-      <UserPanel />
+      {/* <UserPanel /> */}
       <DashboardUnderMenu />
       <User />
     </div>
