743ab22a299894a20bdca5d94a73049990fb07ad
wesselpawel <wesselpawel@wp.pl>
2024-12-01 17:36:27 +0100

    added components

--- src/app/user/Nav.tsx
+++ src/app/user/Nav.tsx
@@ -2,29 +2,20 @@
 import Link from "next/link";
 import {
   FaChevronDown,
-  FaEdit,
   FaHome,
   FaPlusCircle,
-  FaShoppingCart,
   FaSignOutAlt,
   FaTag,
   FaUpload,
 } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import {
-  FaArrowLeft,
-  FaBriefcase,
-  FaCoins,
-  FaList,
-  FaPlus,
-} from "react-icons/fa6";
+import { FaArrowLeft, FaBriefcase, FaCoins, FaList } from "react-icons/fa6";
 import { AiFillThunderbolt } from "react-icons/ai";
 import { usePathname, useRouter } from "next/navigation";
 import { set_modals } from "@/redux/slices/modalsopen";
 import { useDispatch, useSelector } from "react-redux";
 import Settings from "@/components/Dashboard/Settings/Settings";
-import { setUser } from "@/redux/slices/user";
 export default function Nav({
   isNavOpen,
   setNavOpen,
@@ -87,9 +78,14 @@ export default function Nav({
   const dispatch = useDispatch();
   const { modals } = useSelector((state: any) => state.modals);
   const { user } = useSelector((state: any) => state.user);
-  const [source, setSource] = useState(user);
+  const [source, setSource] = useState();
+  useEffect(() => {
+    if (user) {
+      setSource(user);
+    }
+  }, [user]);
   return (
-    <>
+    <div>
       <Settings source={source} setSource={setSource} />
       <div
         className={`!z-[999999999999999999] fixed scrollbar !text-white ${
@@ -257,6 +253,6 @@ export default function Nav({
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -2,8 +2,6 @@
 import { auth } from "@/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import LoginPage from "./LoginPage";
-
-import Loading from "./loading";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import InitUser from "./InitUser";
 import Nav from "./Nav";
@@ -15,22 +13,19 @@ export default function AdminLayout({
 }) {
   const [isNavOpen, setNavOpen] = useState(true);
   const [user, loading] = useAuthState(auth);
-  if (loading) {
-    return <Loading />;
-  } else
-    return (
-      <div className="w-full relative z-[9999] bg-white">
-        {user ? (
-          <>
-            <InitUser user={user} />
-            <Nav setNavOpen={setNavOpen} isNavOpen={isNavOpen} />
-            <div className="pl-[300px] min-w-full min-h-screen bg-[#222430]">
-              {children}
-            </div>
-          </>
-        ) : (
-          <LoginPage />
-        )}
-      </div>
-    );
+  return (
+    <div className="w-full relative z-[9999] bg-white">
+      {user ? (
+        <>
+          <InitUser user={user} />
+          <Nav setNavOpen={setNavOpen} isNavOpen={isNavOpen} />
+          <div className="pl-[300px] min-w-full min-h-screen bg-[#222430]">
+            {children}
+          </div>
+        </>
+      ) : (
+        <LoginPage />
+      )}
+    </div>
+  );
 }
