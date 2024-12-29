26ba6768f7e3da5fe614a3e7096c3ff4fcaf34af
wesselpawel <wesselpawel@wp.pl>
2024-12-29 17:10:43 +0100

    check

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,3 +1,4 @@
+"use server";
 export async function getPosts() {
   const posts = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/posts?tubylytylkofigi=${process.env.API_SECRET_KEY}`
--- src/lib/getServices.ts
+++ src/lib/getServices.ts
@@ -1,3 +1,4 @@
+"use server";
 export async function getServices() {
   const services = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/services?tubylytylkofigi=${process.env.API_SECRET_KEY}`
